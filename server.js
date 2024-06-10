32f268f0d95ce09ee86cc64e7332860cd2ed24f2
kmoslavac <Karlo.Moslavac@infobip.com>
2024-06-11 01:26:01 +0200

    server.js

--- server.js
+++ server.js
@@ -4,27 +4,19 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const userRoutes = require('./routes/users');
 const codeRoutes = require('./routes/codes');
-const path = require('path');
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 1986;
 
 app.use(express.json());
 app.use(cors());
 
-app.use(express.static(path.join(__dirname, '../frontend/dist')));
-
 app.use('/users', userRoutes);
 app.use('/codes', codeRoutes);
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
-});
-
-
 const mongoURI = process.env.MONGO_URI;
 
-mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(mongoURI)
   .then(() => console.log('MongoDB connected: ' + mongoURI))
   .catch(err => console.log('MongoDB connection error: ', err));
 
